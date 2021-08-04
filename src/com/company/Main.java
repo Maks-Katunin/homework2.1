2539542a69cb938f2b274aa05da9d675792809fe
Maks-Katunin <katunin87@gmail.com>
2021-08-04 13:04:22 +0600

    first work

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int age;
+        age = 20;
+        int temp;
+        temp = 20;
+
+        if ((age >= 20 && age <= 45) && (temp >= -20 && temp <= 30)) {
+            System.out.println("Go to walk");
+        } else if (age < 20 && (temp >= 0 && temp <= 28)) {
+            System.out.println("Go to walk");
+        } else if (age > 45 && (temp >= -10 && temp <= 25)) {
+            System.out.println("Go to walk");
+        } else System.out.println("Stay at home");
+
+    }
+    public static int ageAndTemp(      int ageResult, int tempResult){
+        int age
+    }
+
+
+}
